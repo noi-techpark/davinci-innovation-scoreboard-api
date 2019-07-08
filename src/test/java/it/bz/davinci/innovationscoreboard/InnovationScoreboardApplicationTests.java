4da7807a4035111b023bdd8b634e4584a5d42696
Alex Lanz <alex@krumer.it>
2019-07-08 09:14:08 +0200

    Initial setup

---
+++ src/test/java/it/bz/davinci/innovationscoreboard/InnovationScoreboardApplicationTests.java
@@ -0,0 +1,16 @@
+package it.bz.davinci.innovationscoreboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InnovationScoreboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

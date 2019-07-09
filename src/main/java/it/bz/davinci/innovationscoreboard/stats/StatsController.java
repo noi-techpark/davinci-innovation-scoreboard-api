0f82690c6d2f304a11d0179786e82753f9f4f432
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2019-07-09 21:38:36 +0200

    upload CSV file

---
+++ src/main/java/it/bz/davinci/innovationscoreboard/stats/StatsController.java
@@ -0,0 +1,30 @@
+package it.bz.davinci.innovationscoreboard.stats;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class StatsController {
+
+    private final StatsCsvParser statsCsvParser;
+
+    @Autowired
+    public StatsController(StatsCsvParser statsCsvParser) {
+        this.statsCsvParser = statsCsvParser;
+    }
+
+    @PostMapping(value = "/upload/csv")
+    public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        statsCsvParser.parse(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/hello";
+    }
+
+}
---
+++ src/main/java/it/bz/davinci/innovationscoreboard/stats/StatsCsvParser.java
@@ -0,0 +1,15 @@
+package it.bz.davinci.innovationscoreboard.stats;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class StatsCsvParser {
+
+    public void parse(MultipartFile file) {
+
+
+
+    }
+
+}

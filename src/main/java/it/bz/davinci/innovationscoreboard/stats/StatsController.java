a2e9a14f5e4f7c4da8490ef74f5816b54e0cf38f
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2019-07-09 21:49:04 +0200

    parse CSV example

--- src/main/java/it/bz/davinci/innovationscoreboard/stats/StatsController.java
+++ src/main/java/it/bz/davinci/innovationscoreboard/stats/StatsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+
 @Controller
 public class StatsController {
 
@@ -19,7 +21,7 @@ public class StatsController {
     }
 
     @PostMapping(value = "/upload/csv")
-    public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+    public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
         statsCsvParser.parse(file);
         redirectAttributes.addFlashAttribute("message",
                 "You successfully uploaded " + file.getOriginalFilename() + "!");
--- src/main/java/it/bz/davinci/innovationscoreboard/stats/StatsCsvParser.java
+++ src/main/java/it/bz/davinci/innovationscoreboard/stats/StatsCsvParser.java
@@ -1,14 +1,28 @@
 package it.bz.davinci.innovationscoreboard.stats;
 
+import com.opencsv.CSVReader;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 @Component
 public class StatsCsvParser {
 
-    public void parse(MultipartFile file) {
+    public void parse(MultipartFile file) throws IOException {
 
+        try (Reader reader = new InputStreamReader(file.getInputStream());
+             CSVReader csvReader = new CSVReader(reader)) {
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                System.out.println("first name : " + nextRecord[0]);
+                System.out.println("last name : " + nextRecord[1]);
+                System.out.println("==========================");
+            }
 
+        }
 
     }
 

033708d107c79e488031cad7f1c859272d7dc783
Alex Lanz <alex@krumer.it>
2019-09-06 10:18:33 +0200

    Fixing little setup bugfixes

--- src/main/resources/db/migration/V3__createAdminUser.sql
+++ src/main/resources/db/migration/V3__createAdminUser.sql
@@ -1,2 +1,2 @@
-insert into api_user (email, password, enabled, last_update, creation) values ('info@davinici.bz.it', '$2a$10$DKKUcKqQlNk.LaQLAeBZiejD2eG03vG86XeLuIwYKWeInN.Gs1nvm', true, now(), now());
-insert into user_role (email, role) values ('info@davinici.bz.it', 'ROLE_ADMIN');
+insert into api_user (email, password, enabled, last_update, creation) values ('info@davinci.bz.it', '$2a$10$DKKUcKqQlNk.LaQLAeBZiejD2eG03vG86XeLuIwYKWeInN.Gs1nvm', true, now(), now());
+insert into user_role (email, role) values ('info@davinci.bz.it', 'ROLE_ADMIN');

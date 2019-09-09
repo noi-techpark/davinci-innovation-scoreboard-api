efbe7d5867a889b1d39e76a4694f0f76e8cac46d
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2019-09-09 17:07:54 +0200

    Add integration test for web security configuration

--- src/test/java/it/bz/davinci/innovationscoreboard/it/WebSecurityIT.java
+++ src/test/java/it/bz/davinci/innovationscoreboard/it/WebSecurityIT.java
@@ -1,24 +1,28 @@
 package it.bz.davinci.innovationscoreboard.it;
 
+import it.bz.davinci.innovationscoreboard.it.util.Credentials;
+import it.bz.davinci.innovationscoreboard.it.util.LoginResponseBody;
+import it.bz.davinci.innovationscoreboard.user.dto.NewUserRequest;
+import it.bz.davinci.innovationscoreboard.user.dto.UserResponse;
 import it.bz.davinci.innovationscoreboard.user.jpa.UserRepository;
 import it.bz.davinci.innovationscoreboard.user.jpa.UserRoleRepository;
 import it.bz.davinci.innovationscoreboard.user.model.ApiUser;
 import it.bz.davinci.innovationscoreboard.user.model.Role;
 import it.bz.davinci.innovationscoreboard.user.model.UserRole;
+import org.jetbrains.annotations.NotNull;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -63,27 +67,42 @@ public class WebSecurityIT {
 
 
     @Test
-    public void givenValidCredentials_LoginUser() throws Exception {
-        final ResponseEntity<Object> responseEntity = template.postForEntity("/v1/authenticate", new Credentials("info@davinici.bz.it", "password"), Object.class);
+    public void givenValidCredentials_LoginUser() {
+        final ResponseEntity<LoginResponseBody> responseEntity = loginAsAdmin();
         assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
     }
 
-    class Credentials {
-        private String email;
-        private String password;
+    @Test
+    public void givenUnauthenticatedUser_dontAllowAdminProtectedEndpoints() {
+        final NewUserRequest newUserRequest = getNewUserRequest();
 
-        public Credentials(String email, String password) {
-            this.email = email;
-            this.password = password;
-        }
+        final ResponseEntity<UserResponse> createNewUserResponse = template.postForEntity("/v1/users/new", newUserRequest, UserResponse.class);
+        assertThat(createNewUserResponse.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    @Test
+    public void givenAdminToken_allowAdminProtectedEndpoints() {
+        final ResponseEntity<LoginResponseBody> responseEntity = loginAsAdmin();
 
-        public String getPassword() {
-            return password;
-        }
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + responseEntity.getBody().getToken());
+        final NewUserRequest newUserRequest = getNewUserRequest();
+
+        final ResponseEntity<UserResponse> createNewUserResponse = template.postForEntity("/v1/users/new", new HttpEntity<>(newUserRequest, headers), UserResponse.class);
+        assertThat(createNewUserResponse.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(createNewUserResponse.getBody().getEmail(), equalTo(newUserRequest.getEmail()));
+    }
+
+    @NotNull
+    private NewUserRequest getNewUserRequest() {
+        final NewUserRequest newUserRequest = new NewUserRequest();
+        newUserRequest.setPassword("test1234");
+        newUserRequest.setEmail("test@davinci.bz.it");
+        return newUserRequest;
+    }
+
+    private ResponseEntity<LoginResponseBody> loginAsAdmin() {
+        return template.postForEntity("/v1/authenticate", new Credentials("info@davinici.bz.it", "password"), LoginResponseBody.class);
     }
 
 

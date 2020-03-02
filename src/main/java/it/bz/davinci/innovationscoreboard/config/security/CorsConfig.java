1ea7f27db3ad0537b131fe3cbfc3d952d749702a
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2020-03-02 11:26:25 +0100

    enabled CORS

--- src/main/java/it/bz/davinci/innovationscoreboard/config/security/CorsConfig.java
+++ src/main/java/it/bz/davinci/innovationscoreboard/config/security/CorsConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${security.cors.allowedOrigins}")

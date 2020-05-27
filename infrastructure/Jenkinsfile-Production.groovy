a15bea35dac04cd8457b8965a1d0e9db889278dd
Alex Lanz <alex@krumer.it>
2020-05-27 14:21:34 +0200

    Infrastructure folder

--- infrastructure/Jenkinsfile-Production.groovy
+++ infrastructure/Jenkinsfile-Production.groovy
@@ -4,32 +4,32 @@ pipeline {
     environment {
         DOCKER_PROJECT_NAME = "innovation-scoreboard-api"
         DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/innovation-scoreboard-api'
-        DOCKER_TAG = "test-$BUILD_NUMBER"
+        DOCKER_TAG = "prod-$BUILD_NUMBER"
 
-		SERVER_PORT = "1001"
+        SERVER_PORT = "1001"
         LOG_APPLICATION_NAME = "innovation-scoreboard-api"
 
-        POSTGRES_URL = "jdbc:postgresql://test-pg-bdp.co90ybcr8iim.eu-west-1.rds.amazonaws.com:5432/innovation_scoreboard"
-        POSTGRES_USERNAME = credentials('innovation-scoreboard-api-test-postgres-username')
-        POSTGRES_PASSWORD = credentials('innovation-scoreboard-api-test-postgres-password')
+        POSTGRES_URL = "jdbc:postgresql://postgres-prod.co90ybcr8iim.eu-west-1.rds.amazonaws.com:5432/innovation_scoreboard"
+        POSTGRES_USERNAME = credentials('innovation-scoreboard-api-prod-postgres-username')
+        POSTGRES_PASSWORD = credentials('innovation-scoreboard-api-prod-postgres-password')
 
         ELASTICSEARCH_SCHEME = "https"
-        ELASTICSEARCH_HOST = "a2d53654bfcc40d5aa46c18627520e1d.eu-west-1.aws.found.io"
+        ELASTICSEARCH_HOST = "05068ca494804a0b86352e68fadb3457.eu-west-1.aws.found.io"
         ELASTICSEARCH_PORT = "9243"
-        ELASTICSEARCH_USERNAME = credentials('innovation-scoreboard-api-test-elasticsearch-username')
-        ELASTICSEARCH_PASSWORD = credentials('innovation-scoreboard-api-test-elasticsearch-password')
-        ELASTICSEARCH_NAMESPACE_PREFIX = "innovation-scoreboard-test"
+        ELASTICSEARCH_USERNAME = credentials('innovation-scoreboard-api-prod-elasticsearch-username')
+        ELASTICSEARCH_PASSWORD = credentials('innovation-scoreboard-api-prod-elasticsearch-password')
+        ELASTICSEARCH_NAMESPACE_PREFIX = "innovation-scoreboard-prod"
 
         S3_REGION = "eu-west-1"
-        S3_BUCKET_NAME = "test-innovation-scoreboard-api"
-        S3_ACCESS_KEY = credentials('innovation-scoreboard-api-test-s3-access-key')
-        S3_SECRET_KEY = credentials('innovation-scoreboard-api-test-s3-secret-key')
+        S3_BUCKET_NAME = "prod-innovation-scoreboard-api"
+        S3_ACCESS_KEY = credentials('innovation-scoreboard-api-prod-s3-access-key')
+        S3_SECRET_KEY = credentials('innovation-scoreboard-api-prod-s3-secret-key')
 
-        SECURITY_ALLOWED_ORIGINS = "https://innovation.davinci.testingmachine.eu"
-        KEYCLOAK_URL = "https://auth.opendatahub.testingmachine.eu/auth"
+        SECURITY_ALLOWED_ORIGINS = "https://innovation.davinci.bz.it"
+        KEYCLOAK_URL = "https://auth.opendatahub.bz.it/auth"
         KEYCLOAK_REALM = "noi"
         KEYCLOAK_CLIENT_ID = "davinci-innovation-scoreboard-api"
-        KEYCLOAK_CLIENT_SECRET = credentials('innovation-scoreboard-api-test-keycloak-client-secret')
+        KEYCLOAK_CLIENT_SECRET = credentials('innovation-scoreboard-api-prod-keycloak-client-secret')
         KEYCLOAK_SSL_REQUIRED = "none"
     }
 
@@ -43,7 +43,7 @@ pipeline {
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
 
-					echo 'SERVER_PORT=${SERVER_PORT}' >> .env
+                    echo 'SERVER_PORT=${SERVER_PORT}' >> .env
                     echo 'LOG_APPLICATION_NAME=${LOG_APPLICATION_NAME}' >> .env
 
                     echo 'POSTGRES_URL=${POSTGRES_URL}' >> .env
@@ -85,8 +85,8 @@ pipeline {
             steps {
                 sh '''
                     aws ecr get-login --region eu-west-1 --no-include-email | bash
-                    docker-compose --no-ansi -f docker-compose.build.yml build --pull
-                    docker-compose --no-ansi -f docker-compose.build.yml push
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
                 '''
             }
         }
@@ -94,8 +94,8 @@ pipeline {
             steps {
                sshagent(['jenkins-ssh-key']) {
                     sh """
-                        ansible-galaxy install --force -r ansible/requirements.yml
-                        ansible-playbook --limit=test ansible/deploy.yml --extra-vars "build_number=${BUILD_NUMBER}"
+                        ansible-galaxy install -f -r infrastructure/ansible/requirements.yml
+                        ansible-playbook --limit=prod infrastructure/ansible/deploy.yml --extra-vars "release_name=${BUILD_NUMBER}"
                     """
                 }
             }

15e05419d5ec921fd6e77dd478b5c0189216c741
henri <henri@moosbauer.com>
2023-06-01 15:31:31 +0200

    make reuse compliant

--- src/main/java/it/bz/davinci/innovationscoreboard/stats/rest/StatsUploadController.java
+++ src/main/java/it/bz/davinci/innovationscoreboard/stats/rest/StatsUploadController.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 package it.bz.davinci.innovationscoreboard.stats.rest;
 
 import it.bz.davinci.innovationscoreboard.stats.FileImportLogService;
